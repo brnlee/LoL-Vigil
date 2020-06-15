2271fbbafd43204832aa2de294c1f0dab1f72480
brnlee <Brianleenyc@gmail.com>
2020-06-14 20:25:40 -0400

    get_schedule supports pagination

--- backend/getSchedule/main.go
+++ backend/getSchedule/main.go
@@ -1,75 +1,89 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-	"github.com/brnlee/LoL-Vigil/utils"
+	"github.com/brnlee/LoL-Vigil/common"
+	"net/http"
 	"time"
 )
 
-type Cache struct {
+type CachedSchedulePage struct {
 	Schedule        string
 	TimeLastUpdated time.Time
 }
 
 var (
-	ErrNoSchedule = errors.New("failed to get schedule")
+	db = common.ConnectToDynamoDb()
 
-	db = utils.ConnectToDynamoDb()
-
-	cache Cache
+	cache = make(map[string]CachedSchedulePage)
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	cacheAge := time.Now().Sub(cache.TimeLastUpdated)
-	if (cache != Cache{}) && cacheAge.Seconds() < 60 {
-		fmt.Printf("Retrieved schedule from cache. CacheAge is %f\n", cacheAge.Seconds())
+	page, ok := request.QueryStringParameters["page"]
+	if !ok || len(page) == 0 {
+		page = "1"
+	}
+
+	cachedPage, ok := cache[page]
+	if ok {
+		cacheAge := time.Now().Sub(cachedPage.TimeLastUpdated)
+		if cacheAge.Seconds() < 60 {
+			fmt.Printf("Retrieved schedule from cache. CacheAge is %f\n", cacheAge.Seconds())
+			return events.APIGatewayProxyResponse{
+				Body:       cachedPage.Schedule,
+				StatusCode: http.StatusOK,
+				Headers:    map[string]string{"Content-Type": "application/json"},
+			}, nil
+		}
+	}
+
+	schedule, err := getScheduleFromDb(page)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	} else if len(schedule) == 0 {
 		return events.APIGatewayProxyResponse{
-			Body:       cache.Schedule,
-			StatusCode: 200,
+			StatusCode: http.StatusNotFound,
+			Body:       fmt.Sprintf("Schedule page %s was not found", page),
 		}, nil
 	}
 
-	schedule, err := getScheduleFromDb()
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, ErrNoSchedule
-	}
-
-	cache = Cache{
+	cache[page] = CachedSchedulePage{
 		Schedule:        schedule,
 		TimeLastUpdated: time.Now(),
 	}
 
 	return events.APIGatewayProxyResponse{
 		Body:       schedule,
-		StatusCode: 200,
+		StatusCode: http.StatusOK,
+		Headers:    map[string]string{"Content-Type": "application/json"},
 	}, nil
 }
 
-func getScheduleFromDb() (string, error) {
+func getScheduleFromDb(page string) (string, error) {
 	input := &dynamodb.GetItemInput{
-		TableName: aws.String("Matches"),
+		TableName: aws.String("Schedule"),
 		Key: map[string]*dynamodb.AttributeValue{
-			"id": {
-				N: aws.String("-1"),
+			"page": {
+				N: aws.String(page),
 			},
 		},
 	}
 	result, err := db.GetItem(input)
 	if err != nil {
-		utils.CheckDbResponseError(err)
+		common.CheckDbResponseError(err)
 		return "", err
+	} else if result.Item == nil {
+		return "", nil
 	}
 
 	type Item struct {
 		Schedule string `json:"schedule"`
 	}
-
 	item := Item{}
 	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
 	if err != nil {
