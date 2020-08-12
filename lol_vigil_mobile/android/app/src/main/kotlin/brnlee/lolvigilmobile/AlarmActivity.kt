1f01012b2a9213e377c4ed9932f068c259e9b2a3
brnlee <Brianleenyc@gmail.com>
2020-08-12 17:48:53 -0400

    [Frontend] Launch alarm

--- lol_vigil_mobile/android/app/src/main/kotlin/brnlee/lolvigilmobile/AlarmActivity.kt
+++ lol_vigil_mobile/android/app/src/main/kotlin/brnlee/lolvigilmobile/AlarmActivity.kt
@@ -1,26 +1,43 @@
 package brnlee.lolvigilmobile
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
-
-
-
+import android.widget.Button
+import android.widget.TextView
+import io.flutter.Log
 
 class AlarmActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
+
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or
                 WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
                 WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
-                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+        )
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
             setTurnScreenOn(true)
             setShowWhenLocked(true)
         }
-        print("hello123abc123")
+        Log.d("tag", intent.getStringExtra("match"))
+
+        val dismissButton = findViewById<Button>(R.id.dismissButton)
+        dismissButton.setOnClickListener{
+            finish()
+        }
+
+        val alarmTitle = findViewById<TextView>(R.id.alarmTitle)
+        alarmTitle.text = intent.getStringExtra("match")
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val alarmTitle = findViewById<TextView>(R.id.alarmTitle)
+        alarmTitle.text = intent?.getStringExtra("match")
     }
 }
--- lol_vigil_mobile/lib/main.dart
+++ lol_vigil_mobile/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
 import 'package:lolvigilmobile/models/MatchAlarm.dart';
+import 'package:lolvigilmobile/utils/androidHelpers.dart';
 import 'package:lolvigilmobile/widgets/matchList.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:android_intent/android_intent.dart';
 
 class App extends StatelessWidget {
   @override
@@ -17,6 +16,8 @@ class App extends StatelessWidget {
     );
   }
 }
+
+String firebaseToken;
 
 void main() async {
   Hive.registerAdapter(MatchAlarmAdapter());
@@ -32,21 +33,8 @@ void main() async {
 
   _firebaseMessaging.configure(
     onMessage: (Map<String, dynamic> message) async {
-      _getBatteryLevel();
       print("onMessage: $message");
-      AndroidIntent intent = AndroidIntent(
-        action: 'android.intent.action.RUN',
-
-        // Replace this by your package name.
-        package: 'brnlee.lolvigilmobile',
-
-        // Replace this by your package name followed by the activity you want to open.
-        // The default activity provided by Flutter is MainActivity, but you can check
-        // this in AndroidManifest.xml.
-        componentName: 'brnlee.lolvigilmobile.AlarmActivity',
-      );
-
-      await intent.launch();
+      if (message.containsKey('data')) launchAlarm(message['data']);
     },
     onBackgroundMessage: myBackgroundMessageHandler,
     onLaunch: (Map<String, dynamic> message) async {
@@ -57,53 +45,13 @@ void main() async {
     },
   );
 
-
-
   runApp(App());
 }
 
-String firebaseToken;
 
 Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) {
-//  _getBatteryLevel();
   print(message);
 
-  if (message.containsKey('data')) {
-    // Handle data message
-    final dynamic data = message['data'];
-    print(data);
-  }
-
-  if (message.containsKey('notification')) {
-    // Handle notification message
-    final dynamic notification = message['notification'];
-  }
-  AndroidIntent intent = AndroidIntent(
-    action: 'android.intent.action.RUN',
-
-    // Replace this by your package name.
-    package: 'brnlee.lolvigilmobile',
-
-    // Replace this by your package name followed by the activity you want to open.
-    // The default activity provided by Flutter is MainActivity, but you can check
-    // this in AndroidManifest.xml.
-    componentName: 'brnlee.lolvigilmobile.AlarmActivity',
-  );
-
-  intent.launch();
-  // Or do other work.
+  if (message.containsKey('data')) launchAlarm(message['data']);
 }
 
-Future<void> _getBatteryLevel() async {
-  const platform = const MethodChannel('brnlee.lolvigilmobile/alarmScreen');
-  print("GETTING BATTERY LEVEL");
-  String batteryLevel;
-  try {
-    final int result = await platform.invokeMethod('getBatteryLevel');
-    batteryLevel = 'Battery level at $result % .';
-  } on PlatformException catch (e) {
-    batteryLevel = "Failed to get battery level: '${e.message}'.";
-  }
-
-  print("BATTERY LEVEL: $batteryLevel");
-}
